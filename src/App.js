110ad2145d8126e434b4d1bbbc911e6e177fd313
Sumit Maurya <mauryasumit222@gmail.com>
2024-10-23 18:42:01 +0530

    changes_reverted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Sidebar from "./components/Sidebar";
-import Topbar from "./components/Topbar";
-import Dashboard from "./pages/Dashboard";
-import Customers from "./pages/Customers";
-import Messages from "./pages/Messages";
-import Help from "./pages/Help";
-import Settings from "./pages/Settings";
-import Password from "./pages/Password";
-import SignOut from "./pages/SignOut";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Sidebar from './components/Sidebar';
+import Topbar from './components/Topbar';
+import Dashboard from './pages/Dashboard';
+import Customers from './pages/Customers';
+import Messages from './pages/Messages';
+import Help from './pages/Help';
+import Settings from './pages/Settings';
+import Password from './pages/Password';
+import SignOut from './pages/SignOut';
 
 function App() {
   const [menuActive, setMenuActive] = useState(false);
@@ -21,20 +21,18 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <div className="container-fluid">
-          <Sidebar menuActive={menuActive} />
-          <div className={`main ${menuActive ? "menu-active" : ""}`}>
-            <Topbar toggleMenu={toggleMenu} />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/customers" element={<Customers />} />
-              <Route path="/messages" element={<Messages />} />
-              <Route path="/help" element={<Help />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/password" element={<Password />} />
-            </Routes>
-          </div>
+      <div className="container-fluid">
+        <Sidebar menuActive={menuActive} />
+        <div className={`main ${menuActive ? 'menu-active' : ''}`}>
+          <Topbar toggleMenu={toggleMenu} />
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/customers" element={<Customers />} />
+            <Route path="/messages" element={<Messages />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/password" element={<Password />} />
+          </Routes>
         </div>
       </div>
     </Router>
